90be60ef2e586f202d32f1152a4b2c81832d81e8
Volodymyr Petruniak <y.petrunyak@gmail.com>
2018-08-21 01:36:20 +0300

    using system() comands

--- main.c
+++ main.c
@@ -2,14 +2,34 @@
 #include <stdlib.h>
 
 
-int main(int argc, char *argv[], char *env[]) {
-	int i;
-	printf("?????????? ?????????? ????????? ?????? %d \n", argc);
-	printf("????????? ????????? ??????:\n");
-	for (i = 0; i < argc; i++)
-		printf("%s\n", argv[i]);
-	printf("\n????????? ????????? ?????:\n");
-	for (i = 0; env[i] != NULL; i++)
-		printf("%s\n", env[i]);
+int main(int argc, char *argv[], char *env[]) 
+{
+	int choice;
+start:	
+	puts("This is programm that use system command");
+	puts("You can watch which command do what");
+	puts("The list of command:");
+	printf("1 : \"color of letter yellow\"\n2 : \"cls\"\n3 : \"color of background : gray\"\n4 : \"exit\"\n" );
+	
+	while(1)
+	{
+	
+	scanf("%d",&choice);
+	
+	switch(choice)
+	{
+			case 1:system("color e");break;
+			case 2:system("cls");
+			goto start;
+			break;
+			
+			case 3:system("color 80");break;
+			case 4:exit(0);
+			default: puts("Enter digit from 1 to 4");
+	}	
+	
+		fflush(stdin);
+	}
+	
 	return 0;
 }
